cb9beaf511a71cca593141338780e61b1553c8c3
beceluiz <luizfernando.z.l.2011@gmail.com>
2022-08-22 00:00:40 -0300

    refactor

--- src/app.ts
+++ src/app.ts
@@ -2,8 +2,6 @@ import Koa from "koa";
 import Router from "koa-router";
 import logger from "koa-logger";
 import bodyParser from "koa-bodyparser";
-import { connectDB } from "./mongodb";
-import dotenv from "dotenv";
 
 import { GetAllMovies } from "./api/movies/MovieGetAll";
 import { GetMovie } from "./api/movies/GetMovie";
@@ -12,13 +10,10 @@ import { CreateMovie } from "./api/movies/CreateMovie";
 import { UpdateMovie } from "./api/movies/UpdateMovie";
 
 const app = new Koa();
-const index = new Router();
 const router = new Router();
 
 app.use(logger());
 app.use(bodyParser());
-
-// INDEX PAGE
 
 router.get("/", async (ctx) => {
   ctx.body = { message: "Welcome to my first KOA API!" };
