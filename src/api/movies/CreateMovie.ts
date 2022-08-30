0d6e92ed896c2cc89332dceffac3d33bbc5d4f3f
beceluiz <luizfernando.z.l.2011@gmail.com>
2022-08-30 19:11:04 -0300

    error treatment

--- src/api/movies/CreateMovie.ts
+++ src/api/movies/CreateMovie.ts
@@ -2,12 +2,53 @@ import { Context } from "koa";
 import Movies from "../../models/SchemaMovies";
 
 export const CreateMovie = async (ctx: Context) => {
-  try {
-    const movies = await Movies.create(ctx.request.body);
+  const { name, rating, year } = ctx.request.body;
 
-    ctx.body = movies;
-  } catch (err) {
+  if (!name || !rating || !year) {
     ctx.status = 400;
-    ctx.body = { error: "error creating movie" };
+    ctx.body = {
+      message: "all fields required",
+    };
+    return;
+  }
+  if (name.length > 30) {
+    ctx.status = 400;
+    ctx.body = {
+      message: "too long name",
+    };
+    return;
+  }
+  if (rating.length > 2 || rating === "0" || rating > 10) {
+    ctx.status = 400;
+    ctx.body = {
+      message: "you can only rate a movie between 1 and 10",
+    };
+    return;
+  }
+  if (year.length > 4 || year.length < 4) {
+    ctx.status = 400;
+    ctx.body = {
+      message: "a year need to have 4 characters",
+    };
+    return;
+  }
+  try {
+    const movieAlreadyExists = await Movies.findOne({ name: name }).exec();
+
+    if (movieAlreadyExists) {
+      ctx.status = 400;
+      ctx.body = {
+        message: "movie already exists",
+      };
+      return;
+    }
+
+    Movies.create({ name: name, year: year, rating: rating });
+    ctx.status = 201;
+  } catch (error) {
+    ctx.status = 500;
+    ctx.body = {
+      error: "error creating movie",
+    };
   }
 };
